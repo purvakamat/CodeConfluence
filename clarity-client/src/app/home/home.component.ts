097d6ffceabb49af2a7a214956db7727408a039a
Purva Kamat <pkamat@vmware.com>
2017-07-19 21:24:32 -0700

    Initial code setup of clarity client

---
+++ clarity-client/src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2016 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+import { Component } from "@angular/core";
+
+@Component({
+    styleUrls: ['./home.component.scss'],
+    templateUrl: './home.component.html',
+    providers:
+})
+export class HomeComponent {
+    private name: string;
+
+}
